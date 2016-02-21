bf8cdb830b415235c9326489a63b6f53e4ca5681
Valentina <valentinas@appturn.com>
2016-02-21 19:32:18 +0200

    item type added

--- Assets/Scripts/Data/ItemInfo.cs
+++ Assets/Scripts/Data/ItemInfo.cs
@@ -4,8 +4,17 @@ using System.Collections;
 [CreateAssetMenu]
 public class ItemInfo : ScriptableObject
 {
+	public enum EItemType
+	{
+		Socks,
+		FishingRod,
+		Food,
+
+	}
+
 	public GameObject Shape;
 	public float WeightKoeff;
 	public float Weight = 5f;
 	public int Amount = 1;
+	public EItemType Type;
 }
