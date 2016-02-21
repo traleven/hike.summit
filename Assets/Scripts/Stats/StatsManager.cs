7852ed94f8a40f86b27bfe09006886d1de8a0471
cxgr <dmitry.radionov@gmail.com>
2016-02-21 14:10:05 +0200

    stats-hud ties

--- Assets/Scripts/Stats/StatsManager.cs
+++ Assets/Scripts/Stats/StatsManager.cs
@@ -6,56 +6,144 @@ namespace Hike
 {
     public class StatsManager : MonoBehaviour
     {
-        public enum EStat
-        {
-            Stamina,
-            FillFood,
-            FillWater,
+        public const float vitalsMax = 100f;
+        public const float conditionsMax = 100f;
 
-            HealthGeneral,
-            HealthFeet,
+        public const float tempMin = -50f;
+        public const float tempMax = 70f;
 
-            BodyTemp,
-
-            WetnessGeneral,
-            WetnessFeet,
-
-            ResistWind,
-            ResistWeather,
-            ResistFeet,
-            ResistWaterFeet,
-            ResistTemp,
-
-            PackLoad,
-            PackBalance,
-        }
-
+        #region VITALS
         private float stamina;
         public float Stamina
         {
             get { return stamina; }
-            set { stamina = value; }
+            set { stamina = Mathf.Clamp(value, 0f, vitalsMax); }
+        }
+
+        private float fillFood;
+        public float FillFood
+        {
+            get { return fillFood; }
+            set { fillFood = Mathf.Clamp(value, 0f, vitalsMax); }
+        }
+
+        private float fillWater;
+        public float FillWater
+        {
+            get { return fillWater; }
+            set { fillWater = Mathf.Clamp(value, 0f, vitalsMax); }
+        }
+
+        #endregion
+
+        #region CONDITIONS
+        private float painBody;
+        public float PainBody
+        {
+            get { return painBody; }
+            set { painBody = Mathf.Clamp(value, 0f, conditionsMax); }
+        }
+
+        private float painFeet;
+        public float PainFeet
+        {
+            get { return painFeet; }
+            set { painFeet = Mathf.Clamp(value, 0f, conditionsMax); }
+        }
+
+        private float wetnessBody;
+        public float WetnessBody
+        {
+            get { return wetnessBody; }
+            set { wetnessBody = Mathf.Clamp(value, 0f, conditionsMax); }
+        }
+
+        private float wetnessFeet;
+        public float WetnessFeet
+        {
+            get { return wetnessFeet; }
+            set { wetnessFeet = Mathf.Clamp(value, 0f, conditionsMax); }
+        }
+
+        private float playerTemperature;
+        public float PlayerTemperature
+        {
+            get { return playerTemperature; }
+            set { playerTemperature = Mathf.Clamp(value, tempMin, tempMax); }
+        }
+
+        #endregion
+
+        #region BACKPACK
+        private float loadWeight;
+        public float LoadWeight
+        {
+            get { return loadWeight; }
+            set { loadWeight = value; }
+        }
+
+        private float packBalance;
+        public float LoadBalance
+        {
+            get { return packBalance; }
+            set { packBalance = value; }
+        }
+
+        #endregion
+
+        #region RESISTS
+        private float resWind;
+        public float ResWind
+        {
+            get { return resWind; }
+            set { resWind = value; }
+        }
+
+        private float resTemperature;
+        public float ResTemperature
+        {
+            get { return resTemperature; }
+            set { resTemperature = value; }
+        }
+
+        private float resFeetWetness;
+        public float ResFeetWetness
+        {
+            get { return resFeetWetness; }
+            set { resFeetWetness = value; }
+        }
+
+        private float resBodyWetness;
+        public float ResBodyWetness
+        {
+            get { return resBodyWetness; }
+            set { resBodyWetness = value; }
+        }
+
+        private float resFeetPhysical;
+        public float ResFeetPhysical
+        {
+            get { return resFeetPhysical; }
+            set { resFeetPhysical = value; }
+        }
+
+        #endregion
+
 
         public void Init()
         {
-
         }
 
-        public void RecalculateStats()
+        private void RecalculateDerivedStats()
         {
 
         }
 
         public void UpdateStats()
         {
-            RecalculateStats();
-            UpdateHUD();
-        }
-
-
-        public void UpdateHUD()
-        {
-
+            RecalculateDerivedStats();
+            if (null != HUD.Instance)
+                HUD.Instance.UpdateStatsHud(this);
         }
     }
 }
--- Assets/Scripts/UI/HUD.cs
+++ Assets/Scripts/UI/HUD.cs
@@ -2,29 +2,74 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class HUD : MonoBehaviour
+namespace Hike
 {
-    private static HUD instance;
-    public static HUD Instance { get { if (null == instance) instance = FindObjectOfType<HUD>(); return instance; } }
+    public class HUD : MonoBehaviour
+    {
+        private static HUD instance;
+        public static HUD Instance { get { if (null == instance) instance = FindObjectOfType<HUD>(); return instance; } }
 
-    public Slider sldStamina;
-    public Slider sldFood;
-    public Slider sldThirst;
+        public Slider sldStamina;
+        public Slider sldFood;
+        public Slider sldThirst;
 
-    public Slider sldHealthBody;
-    public Slider sldHealthFeet;
-    public Slider sldWetBody;
-    public Slider sldWetFeet;
+        public Slider sldPainBody;
+        public Slider sldPainFeet;
+        public Slider sldWetBody;
+        public Slider sldWetFeet;
 
-    public Text lblTempResist;
-    public Text lblWeatherResist;
-    public Text lblWindResist;
-    public Text lblFeetPhysResist;
-    public Text lblFeetWetResist;
+        public Text lblTempResist;
+        public Text lblWindResist;
+        public Text lblBodyWetResist;
+        public Text lblFeetPhysResist;
+        public Text lblFeetWetResist;
 
-    public Slider sldTemperature;
-    public Text lblTemperature;
+        public Slider sldTemperature;
+        public Text lblTemperature;
 
-    public Text lblPackWeight;
-    public Text lblPackBalance;
+        public Text lblPackWeight;
+        public Text lblPackBalance;
+
+        public void InitHUD(StatsManager sm)
+        {
+            sldStamina.minValue = 0f;
+            sldStamina.maxValue = StatsManager.vitalsMax;
+
+            sldFood.minValue = 0f;
+            sldFood.maxValue = StatsManager.vitalsMax;
+
+            sldThirst.minValue = 0f;
+            sldThirst.maxValue = StatsManager.vitalsMax;
+
+
+            sldPainBody.minValue = 0f;
+            sldPainBody.maxValue = StatsManager.conditionsMax;
+
+            sldPainFeet.minValue = 0f;
+            sldPainFeet.maxValue = StatsManager.conditionsMax;
+
+            sldWetBody.minValue = 0f;
+            sldWetBody.maxValue = StatsManager.conditionsMax;
+
+            sldWetFeet.minValue = 0f;
+            sldWetFeet.maxValue = StatsManager.conditionsMax;
+
+
+            sldTemperature.minValue = StatsManager.tempMin;
+            sldTemperature.maxValue = StatsManager.tempMin;
+            sldTemperature.onValueChanged.AddListener((v) => lblTemperature.text = v.ToString("0.0") + " C");
+        }
+
+        public void UpdateStatsHud(StatsManager sm)
+        {
+            sldStamina.value = sm.Stamina;
+            sldFood.value = sm.FillFood;
+            sldThirst.value = sm.FillWater;
+
+            sldPainBody.value = sm.PainBody;
+            sldPainFeet.value = sm.PainFeet;
+            sldWetBody.value = sm.WetnessBody;
+            sldWetFeet.value = sm.WetnessFeet;
+        }
+    }
 }
