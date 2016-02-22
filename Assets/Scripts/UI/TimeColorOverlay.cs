f5718c8d3c0d6dd5ce7fa19d6f5cff992ed49c07
cxgr <dmitry.radionov@gmail.com>
2016-02-22 08:04:39 +0200

    time-color overlay

---
+++ Assets/Scripts/UI/TimeColorOverlay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeColorOverlay : MonoBehaviour
+{
+    public Slider timeSlider;
+    public Gradient timeGrad;
+    public Image overlaySprite;
+
+    void Update()
+    {
+        if (null != timeSlider && null != overlaySprite)
+            overlaySprite.color = timeGrad.Evaluate(timeSlider.normalizedValue);
+    }
+}
