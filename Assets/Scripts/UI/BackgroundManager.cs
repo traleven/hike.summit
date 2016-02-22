2ba518f4aaf25a34e9f72cb43d2c998754f932be
cxgr <dmitry.radionov@gmail.com>
2016-02-22 03:51:28 +0200

    merge fix

--- Assets/Scripts/UI/BackgroundManager.cs
+++ Assets/Scripts/UI/BackgroundManager.cs
@@ -6,7 +6,11 @@ namespace Hike
 {
 	public class BackgroundManager : MonoBehaviour
 	{
-		[SerializeField] private Image[] layers = new Image[2];
+		[SerializeField] private RawImage[] layers = new RawImage[2];
+
+        public static BackgroundManager Instance { get { return SingletonUtils<BackgroundManager>.Instance; } }
+
+        public RawImage ActiveBG { get { return layers[currentLayer]; } }
 
 		private int currentLayer;
 		private int nextLayer
@@ -21,20 +25,31 @@ namespace Hike
 
 		public void SetBackground(Sprite bg)
 		{
-			if (bg == null || bg == layers[currentLayer].sprite)
+			if (bg == null || bg == layers[currentLayer].texture)
 				return;
 
-			if (layers[currentLayer].sprite == null)
+			if (layers[currentLayer].texture == null)
 			{
-				layers[currentLayer].sprite = bg;
+				layers[currentLayer].texture = bg.texture;
 				return;
 			}
 
 			layers[currentLayer].transform.SetSiblingIndex(0);
 
-			layers[nextLayer].sprite = bg;
+			layers[nextLayer].texture = bg.texture;
 			layers[nextLayer].CrossFadeAlpha(1f, WindowManager.TransitionTime, true);
 			layers[currentLayer].CrossFadeAlpha(0f, WindowManager.TransitionTime, true);
+
+            var screenAspect = Screen.width / Screen.height;
+            var textureAspect = bg.textureRect.width / bg.textureRect.height;
+
+            if (textureAspect >= screenAspect)
+                layers[nextLayer].rectTransform.sizeDelta = new Vector2(Screen.height * textureAspect, Screen.height);
+            else
+                layers[nextLayer].rectTransform.sizeDelta = new Vector2(Screen.width, Screen.width / textureAspect);
+
+            layers[nextLayer].uvRect = new Rect(0f, 0f, 1f, 1f);
+
 			currentLayer = nextLayer;
 		}
 	}
